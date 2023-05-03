5e87576b35b6fb2a5732bdf587c9573f13f23253
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-03 12:16:03 -0700

    remove outer div

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,22 +3,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="p-4">
-      <div className="m-auto flex h-96 max-w-md flex-col items-center justify-center gap-6">
-        <h2 className="text-center">
-          Curate a workout playlist just for you to make hittin' the gym next
-          time that much easier 💯
-        </h2>
-        <ul className="">
-          <li>⭐️ Browse workout videos from YouTube</li>
-          <li>⭐️ Easily take notes on the exercises involved</li>
-          <li>⭐️ Save the workout for easy future reference</li>
-          {/* <li>⭐️ Create a personal playlist of awesome workouts</li> */}
-        </ul>
-        <Link className="btn mt-1 drop-shadow-lg" to="/register">
-          Sign Up Now!
-        </Link>
-      </div>
+    <div className="m-auto flex h-96 max-w-md flex-col items-center justify-center gap-6">
+      <h2 className="text-center">
+        Curate a workout playlist just for you to make hittin' the gym next time
+        that much easier 💯
+      </h2>
+      <ul className="">
+        <li>⭐️ Browse workout videos from YouTube</li>
+        <li>⭐️ Easily take notes on the exercises involved</li>
+        <li>⭐️ Save the workout for easy future reference</li>
+        {/* <li>⭐️ Create a personal playlist of awesome workouts</li> */}
+      </ul>
+      <Link className="btn mt-1 drop-shadow-lg" to="/register">
+        Sign Up Now!
+      </Link>
     </div>
   );
 };
