5d8735885a67b408fffc59d5ee1b8911afa7a703
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-02 23:29:50 -0700

    basic homepage layout

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>This is the Home Page!</div>;
+  return (
+    <div className="p-4">
+      <div className="m-auto flex h-96 max-w-md flex-col items-center justify-center gap-6">
+        <h2 className="text-center">
+          Curate a workout playlist just for you to make hittin' the gym next
+          time that much easier 💯
+        </h2>
+        <ul className="">
+          <li>⭐️ Browse workout videos from YouTube</li>
+          <li>⭐️ Easily take notes on the exercises involved</li>
+          <li>⭐️ Save the workout for easy future reference</li>
+          {/* <li>⭐️ Create a personal playlist of awesome workouts</li> */}
+        </ul>
+        <Link className="btn mt-1 drop-shadow-lg" to="/register">
+          Sign Up Now!
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
