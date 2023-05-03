6c29734c156d200509ed8520fef09628c64678d6
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-02 18:06:04 -0700

    Add layout route

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -1,5 +1,5 @@
 // this file contains all the routes of my application
-import { Route } from "react-router-dom";
+import { Outlet, Route } from "react-router-dom";
 
 // pages
 import Home from "@src/pages/Home/Home";
@@ -7,9 +7,23 @@ import Register from "./src/pages/Register/Register";
 import Login from "./src/pages/Login/Login";
 import CreateWorkout from "./src/pages/CreateWorkout/CreateWorkout";
 import WorkoutCard from "./src/pages/WorkoutCard/WorkoutCard";
+import Navbar from "./src/components/Navbar.jsx/Navbar";
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
 const Routes = (
-  <Route path="/">
+  <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="register" element={<Register />} />
     <Route path="login" element={<Login />} />
