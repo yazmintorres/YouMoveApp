93064beb340e89a1f6ab2222e55cc14d6af54969
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-02 19:10:23 -0700

    create navbar component

--- client/src/components/Navbar.jsx/Navbar.jsx
+++ client/src/components/Navbar.jsx/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // get path
+  const location = useLocation();
+  console.log(location);
+
+  return (
+    <nav>
+      <h1>Welcome to YouMove</h1>
+      {location.pathname === "/" && <NavLink to="/login">Login</NavLink>}
+      {location.pathname === "/login" && <NavLink to="/">Home</NavLink>}
+      {location.pathname === "/register" && <NavLink to="/">Home</NavLink>}
+      {/* {location.pathname === "/create" && <NavLink to="/create">Home</NavLink>} */}
+    </nav>
+  );
 };
 
 export default Navbar;
