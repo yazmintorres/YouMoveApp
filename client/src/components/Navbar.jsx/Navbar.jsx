8aab2cd6438ba68d1ebceb6690b60fe5cedb8513
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-02 23:42:04 -0700

    add media queries for screens larger than 640px

--- client/src/components/Navbar.jsx/Navbar.jsx
+++ client/src/components/Navbar.jsx/Navbar.jsx
@@ -8,16 +8,13 @@ const Navbar = () => {
 
   return (
     <nav className=" bg-gradient-to-r from-blue-600 to-violet-600 p-4 tracking-wider text-white ">
-      <div className="m-auto flex w-11/12 flex-wrap items-center">
-        <h1 className="grow-[1] text-center text-3xl font-bold">
+      <div className="m-auto flex w-11/12 flex-wrap items-center gap-3 sm:gap-0 ">
+        <h1 className="grow-[1] text-center text-3xl font-bold sm:text-left">
           Welcome to <span className="tracking-widest">YouMove</span>
         </h1>
-        <div className=" grow-[2] basis-48 text-center ">
+        <div className=" flex  grow-[2] basis-52 justify-center text-center sm:justify-end ">
           {location.pathname === "/" && (
-            <Link
-              className="w-10 border-b-2 border-solid border-white"
-              to="/login"
-            >
+            <Link className="btn" to="/login">
               Login
             </Link>
           )}
