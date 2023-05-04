190aa08b438fd9c2947bf4f59bc5d267a23f1cc7
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-03 23:54:46 -0700

    create add exercise component form

---
+++ client/src/pages/CreateWorkout/components/AddExercise.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddExercise = () => {
+  return (
+    <div className="w-4/5 rounded-xl border-2 border-solid border-black text-center">
+      <h3 className=" my-4 text-xl font-bold ">Add Exercise</h3>
+    </div>
+  );
+};
+
+export default AddExercise;
