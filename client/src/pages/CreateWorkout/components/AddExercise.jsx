f6d0c7ac2ba6dac63c7cbe6fc32668632c3c8dff
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-04 00:01:02 -0700

    reduce font-size for add exercise form

--- client/src/pages/CreateWorkout/components/AddExercise.jsx
+++ client/src/pages/CreateWorkout/components/AddExercise.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AddExercise = () => {
   return (
     <div className="w-4/5 rounded-xl border-2 border-solid border-black text-center">
-      <h3 className=" my-4 text-xl font-bold ">Add Exercise</h3>
+      <h3 className=" my-4 text-lg font-bold ">Add Exercise</h3>
     </div>
   );
 };
