0edfe91dbf7b67a0bbe1e49fc53fc9d32cd9c613
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-04 14:03:22 -0700

    Add mock API response data

---
+++ client/src/data/search-response.js
@@ -0,0 +1,191 @@
+const searchResponse = {
+  kind: "youtube#searchListResponse",
+  etag: "YtYoxLrLehCPpP9_NMQKkJ1fpXY",
+  nextPageToken: "CAUQAA",
+  regionCode: "US",
+  pageInfo: {
+    totalResults: 1000000,
+    resultsPerPage: 5,
+  },
+  items: [
+    {
+      kind: "youtube#searchResult",
+      etag: "LWOVziLVl0y3vEucUrFvwIckRSU",
+      id: {
+        kind: "youtube#video",
+        videoId: "w8cSjkXkYRc",
+      },
+      snippet: {
+        publishedAt: "2021-12-01T05:00:31Z",
+        channelId: "UCpis3RcTw6t47XO0R_KY4WQ",
+        title: "20 Minute Shoulder Workout with Dumbbells | Caroline Girvan",
+        description:
+          "20 minutes for a solid shoulder session that will work all of the muscles of the shoulders including front, side and rear delts!",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/w8cSjkXkYRc/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/w8cSjkXkYRc/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/w8cSjkXkYRc/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Caroline Girvan",
+        liveBroadcastContent: "none",
+        publishTime: "2021-12-01T05:00:31Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "xzsaEaNf5zMesD4UrXK9ehR8jXs",
+      id: {
+        kind: "youtube#video",
+        videoId: "ME0cj3FTbms",
+      },
+      snippet: {
+        publishedAt: "2020-12-04T21:07:02Z",
+        channelId: "UCpis3RcTw6t47XO0R_KY4WQ",
+        title: "20 MIN SOLID ARMS &amp; SHOULDER WORKOUT with Dumbbells",
+        description:
+          "Let's strengthen the shoulders, triceps and biceps in this 20 minute upper body workout! I have included shoulders in this arm ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/ME0cj3FTbms/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/ME0cj3FTbms/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/ME0cj3FTbms/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Caroline Girvan",
+        liveBroadcastContent: "none",
+        publishTime: "2020-12-04T21:07:02Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "Embgr4BzGEwCsAP_7dc2corgQgg",
+      id: {
+        kind: "youtube#video",
+        videoId: "Bl28i6fWljU",
+      },
+      snippet: {
+        publishedAt: "2021-12-08T05:00:04Z",
+        channelId: "UCpis3RcTw6t47XO0R_KY4WQ",
+        title:
+          "20 Minute Arms and Shoulders Workout with Dumbbells | Caroline Girvan",
+        description:
+          "20 MINUTE SHOULDERS & ARMS workout! Here we go!! Ready for my style of finisher? The triceps will be officially done! Slow ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/Bl28i6fWljU/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/Bl28i6fWljU/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/Bl28i6fWljU/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Caroline Girvan",
+        liveBroadcastContent: "none",
+        publishTime: "2021-12-08T05:00:04Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "VhX9Fc0Vbe0bfyltGvz8mph9Dog",
+      id: {
+        kind: "youtube#video",
+        videoId: "wH-YPotqDlg",
+      },
+      snippet: {
+        publishedAt: "2020-08-12T16:32:19Z",
+        channelId: "UCpis3RcTw6t47XO0R_KY4WQ",
+        title: "20 Min INTENSE DUMBBELL SHOULDER WORKOUT at Home | Supersets",
+        description:
+          "Shoulder supersets! 20 minutes shoulder workout at home using only a pair of dumbbells and a mat! We will hit shoulders at all ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/wH-YPotqDlg/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/wH-YPotqDlg/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/wH-YPotqDlg/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Caroline Girvan",
+        liveBroadcastContent: "none",
+        publishTime: "2020-08-12T16:32:19Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "idGrDtWAj3cI_rhOFjK087tgK5M",
+      id: {
+        kind: "youtube#video",
+        videoId: "P7k7ABUKmxo",
+      },
+      snippet: {
+        publishedAt: "2021-05-25T04:00:11Z",
+        channelId: "UCsLF0qPTpkYKq81HsjgzhwQ",
+        title:
+          "20 MIN TONED ARMS + SHOULDER Workout With Weights - No Repeat, Upper Body Dumbbell Workout",
+        description:
+          "Tone, sculpt and define your Arms and Shoulders with my new 20-minute Dumbbell Workout. Follow along and together we can ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/P7k7ABUKmxo/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/P7k7ABUKmxo/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/P7k7ABUKmxo/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "growingannanas",
+        liveBroadcastContent: "none",
+        publishTime: "2021-05-25T04:00:11Z",
+      },
+    },
+  ],
+};
+
+export default searchResponse;
