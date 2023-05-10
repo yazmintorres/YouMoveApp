afce4c3dc782ebb1e7f568dce9e46c977ff76911
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-10 14:56:34 -0700

    user can see exercise card info associated with a specific video

--- client/src/pages/WorkoutCard/WorkoutCard.jsx
+++ client/src/pages/WorkoutCard/WorkoutCard.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import VideoCard from "@client/src/components/VideoCard/VideoCard";
 import { useLocation } from "react-router-dom";
+import ExerciseCard from "@client/src/components/ExerciseCard/ExerciseCard";
 
 const WorkoutCard = () => {
   const location = useLocation();
   const { workoutInfo } = location.state;
   console.log(workoutInfo);
+  const exerciseCards = workoutInfo.exercises.map((exercise, index) => (
+    <ExerciseCard
+      key={index}
+      number={index + 1}
+      name={exercise.name}
+      durationMinutes={exercise.durationMinutes}
+      durationSeconds={exercise.durationSeconds}
+      sets={exercise.sets}
+      reps={exercise.reps}
+      weight={exercise.weight}
+    />
+  ));
+
   return (
-    <div className="grid grid-cols-2">
-      <h2 className="col-span-full my-4 text-2xl font-bold tracking-wide">
-        Your Saved Workout Card
+    <div className="flex flex-col gap-2">
+      <h2 className=" my-0 mt-4  font-bold tracking-wide">
+        {workoutInfo.title}
       </h2>
-      <VideoCard sourceId="YJPSR9dEQV8" />
+      <div className="border border-solid border-gray-500"></div>
+      <div className="mt-2 md:flex">
+        <div className="flex w-full grow flex-col gap-3">
+          <VideoCard
+            videoId={workoutInfo.video_id}
+            channelTitle={workoutInfo.channel_title}
+            // title={videoInfo.title}
+          />
+        </div>
+        <div className="flex w-full grow flex-col items-center gap-3">
+          {exerciseCards}
+        </div>
+      </div>
     </div>
   );
 };
