79807cd44634129bbc3fa23dc5703efedc24accd
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-12 11:42:54 -0700

    clean up comments

--- client/src/pages/CreateWorkout/CreateWorkout.jsx
+++ client/src/pages/CreateWorkout/CreateWorkout.jsx
@@ -245,25 +245,6 @@ const CreateWorkout = () => {
               handleShowForm={handleShowForm}
             />
           )}
-          {/* {showForm ? (
-            <ExerciseForm
-              exerciseFromParent={exercise}
-              handleExerciseAdded={handleExerciseAdded}
-              handleShowForm={handleShowForm}
-            />
-          ) : (
-            <div
-              onClick={handleAddExercise}
-              className=" w-11/12 rounded-lg border-2 border-solid border-black text-center sm:mt-0 sm:w-4/5"
-            >
-              <div className="m-auto p-3">
-                <h3 className="my-0 flex items-center justify-center gap-2 font-bold">
-                  <MdAddCircle />
-                  Add Exercise
-                </h3>
-              </div>
-            </div>
-          )} */}
         </div>
       </div>
     </div>
