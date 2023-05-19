f6fa577f1a512780a22481023559a6ad09ccb91e
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-19 15:53:34 -0700

    update query parameters

--- client/src/apis/YouTubeAPI.js
+++ client/src/apis/YouTubeAPI.js
@@ -1,8 +1,9 @@
 export async function getSearchVideos(userSearchTerm, pageToken) {
   try {
+    // if they added workut in their query then don't add this
     const searchQuery = userSearchTerm + "workouts women";
     const youtubeKey = import.meta.env.VITE_YOUTUBE_KEY;
-    let url = `https://www.googleapis.com/youtube/v3/search?key=${youtubeKey}&part=snippet&q=${searchQuery}&type=video&chart=mostPopular&maxResults=4`;
+    let url = `https://www.googleapis.com/youtube/v3/search?key=${youtubeKey}&part=snippet&q=${searchQuery}&type=video&chart=mostPopular&maxResults=3`;
     if (pageToken) {
       url = `${url}&pageToken=${pageToken}`;
     }
