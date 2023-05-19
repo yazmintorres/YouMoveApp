cf77bd2db9ab6b41f1368cfa30c593b3f66244fd
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-18 20:25:43 -0700

    form wraps entire component

--- client/src/pages/CreateWorkout/components/ExerciseForm/ExerciseForm.jsx
+++ client/src/pages/CreateWorkout/components/ExerciseForm/ExerciseForm.jsx
@@ -45,8 +45,12 @@ const ExerciseForm = ({
   };
 
   return (
-    <div className="w-11/12 rounded-lg border-2 border-solid border-black text-center sm:mt-0 sm:w-4/5">
-      <div className="m-auto w-11/12">
+    <form
+      aria-label="exercise-form"
+      onSubmit={handleSubmit}
+      className=" rounded-lg border-2 border-solid border-black text-center sm:mt-0 sm:w-4/5 "
+    >
+      <div className="m-auto flex w-11/12 flex-col gap-2">
         <div className="relative my-4">
           <h3 className=" my-0 text-lg font-bold ">
             {exerciseToEdit ? "Edit Exercise" : "Add Exercise"}
@@ -60,111 +64,103 @@ const ExerciseForm = ({
             className="absolute right-1 top-1 text-3xl"
           />
         </div>
-        <form
-          aria-label="exercise-form"
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-2"
-        >
-          <div className="flex flex-wrap items-center justify-between gap-3  ">
-            <label
-              htmlFor="exercise-name"
-              className="w-1/5 basis-auto text-left"
-            >
-              Name*
-            </label>
-            <input
-              className="input-field  "
-              type="text"
-              name="name"
-              id="name"
-              value={exercise.name}
-              onChange={handleChange}
-              maxLength={35}
-              required
-            ></input>
-          </div>
-          <div className="flex flex-wrap items-center justify-between gap-3  ">
-            <label htmlFor="duration" className="w-1/5 basis-auto text-left">
-              Duration
-            </label>
 
-            <input
-              className="input-field w-20  basis-auto"
-              type="number"
-              name="durationMinutes"
-              min={0}
-              max={30}
-              value={exercise.durationMinutes}
-              placeholder="Minutes"
-              onChange={handleChange}
-              id="duration"
-            ></input>
+        <div className="flex flex-wrap items-center justify-between gap-3  ">
+          <label htmlFor="exercise-name" className="w-1/5 basis-auto text-left">
+            Name*
+          </label>
+          <input
+            className="input-field  "
+            type="text"
+            name="name"
+            id="name"
+            value={exercise.name}
+            onChange={handleChange}
+            maxLength={35}
+            required
+          ></input>
+        </div>
+        <div className="flex flex-wrap items-center justify-between gap-3  ">
+          <label htmlFor="duration" className="w-1/5 basis-auto text-left">
+            Duration
+          </label>
 
-            <p>:</p>
+          <input
+            className="input-field w-20  basis-auto"
+            type="number"
+            name="durationMinutes"
+            min={0}
+            max={30}
+            value={exercise.durationMinutes}
+            placeholder="Minutes"
+            onChange={handleChange}
+            id="duration"
+          ></input>
 
-            <input
-              className="input-field  basis-auto sm:w-20"
-              type="number"
-              placeholder="Seconds"
-              id="duration"
-              name="durationSeconds"
-              value={exercise.durationSeconds}
-              onChange={handleChange}
-              min={0}
-              max={60}
-            ></input>
-          </div>
+          <p>:</p>
 
-          <div className="flex flex-wrap items-center justify-between gap-3  ">
-            <label htmlFor="weight" className="w-1/5 basis-auto  text-left">
-              Weight
-            </label>
-            <input
-              className="input-field "
-              type="number"
-              name="weight"
-              id="weight"
-              value={exercise.weight}
-              onChange={handleChange}
-              min={0}
-              placeholder="Pounds"
-            ></input>
-          </div>
+          <input
+            className="input-field  basis-auto sm:w-20"
+            type="number"
+            placeholder="Seconds"
+            id="duration"
+            name="durationSeconds"
+            value={exercise.durationSeconds}
+            onChange={handleChange}
+            min={0}
+            max={60}
+          ></input>
+        </div>
 
-          <div className="flex flex-wrap items-center justify-between gap-3  ">
-            <label htmlFor="sets" className=" w-1/5 basis-auto  text-left">
-              Sets
-            </label>
-            <input
-              className="input-field"
-              type="number"
-              name="sets"
-              id="sets"
-              min={0}
-              value={exercise.sets}
-              onChange={handleChange}
-            ></input>
-          </div>
-          <div className="flex flex-wrap items-center justify-between gap-3  ">
-            <label htmlFor="reps" className=" w-1/5 basis-auto  text-left">
-              Reps
-            </label>
-            <input
-              className="input-field "
-              type="number"
-              name="reps"
-              id="reps"
-              value={exercise.reps}
-              onChange={handleChange}
-              min={0}
-            ></input>
-          </div>
-          <button type="submit" className=" btn-actions my-4 self-center px-4">
-            {exerciseToEdit ? "Update" : "Add"}
-          </button>
-        </form>
+        <div className="flex flex-wrap items-center justify-between gap-3  ">
+          <label htmlFor="weight" className="w-1/5 basis-auto  text-left">
+            Weight
+          </label>
+          <input
+            className="input-field "
+            type="number"
+            name="weight"
+            id="weight"
+            value={exercise.weight}
+            onChange={handleChange}
+            min={0}
+            placeholder="Pounds"
+          ></input>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-between gap-3  ">
+          <label htmlFor="sets" className=" w-1/5 basis-auto  text-left">
+            Sets
+          </label>
+          <input
+            className="input-field"
+            type="number"
+            name="sets"
+            id="sets"
+            min={0}
+            value={exercise.sets}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div className="flex flex-wrap items-center justify-between gap-3  ">
+          <label htmlFor="reps" className=" w-1/5 basis-auto  text-left">
+            Reps
+          </label>
+          <input
+            className="input-field "
+            type="number"
+            name="reps"
+            id="reps"
+            value={exercise.reps}
+            onChange={handleChange}
+            min={0}
+          ></input>
+        </div>
+        <button type="submit" className=" btn-actions my-4 self-center px-4">
+          {exerciseToEdit ? "Update" : "Add"}
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
