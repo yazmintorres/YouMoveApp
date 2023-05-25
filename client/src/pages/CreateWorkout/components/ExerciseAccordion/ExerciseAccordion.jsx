f0a113bbd09d71b29dec611192fd710c9b8920d6
Yazmin <yazminmoniquetorres@gmail.com>
2023-05-25 11:33:23 -0700

    add css styling to center exercise name and information

--- client/src/pages/CreateWorkout/components/ExerciseAccordion/ExerciseAccordion.jsx
+++ client/src/pages/CreateWorkout/components/ExerciseAccordion/ExerciseAccordion.jsx
@@ -58,15 +58,10 @@ const ExerciseAccordion = ({
     <>
       <div className="relative flex w-full flex-col items-center rounded-lg border-2 border-solid border-black text-center md:mt-0 md:w-4/5">
         {/* <div className=" relative m-auto flex w-full items-center justify-center p-3 pb-1"> */}
-        <h3 className="absolute left-0 my-0 w-12 basis-auto font-bold">
+        <h3 className="absolute left-0 my-1 w-12 basis-auto font-bold">
           {exerciseNumber}
         </h3>
-
-        <h3 className=" my-0 grow font-bold">
-          {exercise.name[0].toUpperCase() + exercise.name.slice(1)}
-        </h3>
-
-        <div className="absolute right-0 flex gap-2">
+        <div className="absolute right-0 my-1 flex gap-2">
           <MdModeEdit
             role="button"
             aria-label="edit"
@@ -82,21 +77,28 @@ const ExerciseAccordion = ({
 
           <MdDeleteForever onClick={handleDeleteClick} className="text-xl" />
         </div>
-        {/* </div> */}
 
-        <p>{weightString}</p>
-        <p>
-          {setsString && repsString
-            ? `${setsString} x ${repsString}`
-            : `${setsString} ${repsString}`}
-        </p>
-        <p>{`${minutesString} ${secondsString}`}</p>
-        <div className="my-1 flex w-full justify-center gap-1">
-          {exercise.sets > 0 && <p>Set:</p>}
-          {checkboxes}
-          <button className="ml-1 flex items-center rounded  border-2 border-solid border-blue-900 bg-yellow-300 px-1 text-sm hover:bg-yellow-500 ">
-            Done <MdStar />
-          </button>
+        <div className="flex flex-col items-center">
+          <h3 className=" my-1 grow font-bold">
+            {exercise.name[0].toUpperCase() + exercise.name.slice(1)}
+          </h3>
+
+          {/* </div> */}
+
+          <p>{weightString}</p>
+          <p>
+            {setsString && repsString
+              ? `${setsString} x ${repsString}`
+              : `${setsString} ${repsString}`}
+          </p>
+          <p>{`${minutesString} ${secondsString}`}</p>
+          <div className="my-1 flex w-full justify-center gap-1">
+            {exercise.sets > 0 && <p>Set:</p>}
+            {checkboxes}
+            <button className="ml-1 flex items-center rounded  border-2 border-solid border-blue-900 bg-yellow-300 px-1 text-sm hover:bg-yellow-500 ">
+              Done <MdStar />
+            </button>
+          </div>
         </div>
       </div>
       {showForm && (
